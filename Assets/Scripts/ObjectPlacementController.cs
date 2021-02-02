324cd3aaa930eb36b48fc475b731a0f95aaad2c3
Miral Songhela <songhela@gmail.com>
2021-02-02 16:58:59 +0530

    Initial Commit

---
+++ Assets/Scripts/ObjectPlacementController.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class ObjectPlacementController : MonoBehaviour
+{
+    public GameObject placementIndictor;
+    public GameObject spawnObject;
+
+  
+    public GameObject machine, cor;
+
+    public GameObject[] uiElements;
+
+    private ARRaycastManager aRRaycastManager;
+    private Pose placementPose;
+
+    private float rotationSpeed;
+
+    private bool isPlacementPoseValid, canRotate, isObjectPlaced;
+
+    private static ObjectPlacementController instance;
+
+    public static ObjectPlacementController Instance
+    {
+        get
+        {
+            if(instance == null)
+            {
+                instance = FindObjectOfType<ObjectPlacementController>();
+            }
+            return instance;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        canRotate = isObjectPlaced = false;
+        placementIndictor.SetActive(false);
+        spawnObject.SetActive(false);
+        aRRaycastManager = transform.GetComponent<ARRaycastManager>();
+
+        rotationSpeed = 10f;
+
+        foreach(GameObject go in uiElements)
+        {
+            go.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!isObjectPlaced)
+        {
+            UpdatePlacementPose();
+            UpdatePlacementIndicator();
+
+            if (isPlacementPoseValid && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                placementIndictor.SetActive(false);
+                spawnObject.SetActive(true);
+                spawnObject.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
+                isObjectPlaced = true;
+
+                foreach (GameObject go in uiElements)
+                {
+                    go.SetActive(true);
+                }
+            }
+
+            if (isPlacementPoseValid && canRotate)
+            {
+                machine.transform.RotateAround(cor.transform.position, rotationSpeed * Time.deltaTime);
+            }
+        }
+    }
+
+    private void UpdatePlacementPose()
+    {
+        var screenCenter = Camera.main.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+
+        List<ARRaycastHit> hits = new List<ARRaycastHit>();
+        aRRaycastManager.Raycast(screenCenter, hits, UnityEngine.XR.ARSubsystems.TrackableType.Planes);
+
+        isPlacementPoseValid = hits.Count > 0;
+
+        if(isPlacementPoseValid)
+        {
+            placementPose = hits[0].pose;
+
+            var cameraFarward = Camera.main.transform.forward;
+            var cameraBearing = new Vector3(cameraFarward.x, 0, cameraFarward.z).normalized;
+
+            placementPose.rotation = Quaternion.LookRotation(cameraBearing);
+        }
+    }
+
+    private void UpdatePlacementIndicator()
+    {
+        if(isPlacementPoseValid)
+        {
+            placementIndictor.SetActive(true);
+
+            placementIndictor.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
+        }
+        else
+        {
+            placementIndictor.SetActive(false);
+        }
+    }
+
+    public void ControlRotation()
+    {
+        canRotate = !canRotate;
+        // false  = !false (true)
+        // 2nd time
+
+        // true = !true (false)
+    }
+}
---
+++ Assets/Scripts/SceneBehaviourController.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneBehaviourController : MonoBehaviour
+{
+    public GameObject colorCube;
+    public Animator animCube;
+
+    public GameObject[] objects;
+
+    public RawImage ssPreview;
+
+    private Texture2D ss;
+
+    public GameObject logo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (GameObject go in objects)
+        {
+            go.SetActive(false);
+        }
+        ssPreview.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void ChangeColor(string _color)
+    {
+        Color col;
+        ColorUtility.TryParseHtmlString(_color, out col);
+        colorCube.GetComponent<Renderer>().material.color = col;
+    }
+
+    public void StartAnim()
+    {
+        animCube.Play("RotationAnimation");
+
+        gameObject.GetComponent<AudioSource>().Play();
+
+    }
+
+    public void ChangeObjects(int _index)
+    {
+        foreach(GameObject go in objects)
+        {
+            go.SetActive(false);
+        }
+
+        objects[_index].SetActive(true);
+    }
+
+    public void TakeSnapshot()
+    {
+        GameObject[] gos = ObjectPlacementController.Instance.uiElements;
+
+        foreach(GameObject go in gos)
+        {
+            go.SetActive(false);
+        }
+
+        logo.SetActive(true);
+        StartCoroutine(TakeSS());
+    }
+
+    IEnumerator TakeSS()
+    {
+        yield return new WaitForEndOfFrame();
+
+        ss = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+
+        ss.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+
+        ss.Apply();
+
+        ssPreview.gameObject.SetActive(true);
+        ssPreview.texture = ss;
+
+        //Destroy(ss);
+    }
+
+    public void CancelSanpshot()
+    {
+        GameObject[] gos = ObjectPlacementController.Instance.uiElements;
+
+        foreach (GameObject go in gos)
+        {
+            go.SetActive(true);
+        }
+        ssPreview.gameObject.SetActive(false);
+
+        logo.SetActive(false);
+
+        Destroy(ss);
+    }
+
+    public void ShareSnapshot()
+    {
+        StartCoroutine(ShareSS());
+    }
+
+    IEnumerator ShareSS()
+    {
+        yield return new WaitForEndOfFrame();
+
+        string timeStamp = System.DateTime.Now.ToString("dd-mm-yyyy_HH:mm:ss");
+        string name = $"AR-App-SS {timeStamp}";
+        new NativeShare().AddFile(ss, name).SetTitle("Share your AR experience").SetSubject("This is a nice AR App").SetText("This is a nice AR App").Share();
+
+        CancelSanpshot();
+    }
+}
+
